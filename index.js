430853346cabd7a6a88a05d48bfc4b708db7551c
Guillermo Roque <roquememo@hotmail.com>
2018-08-20 16:29:15 -0600

    Editando API

--- index.js
+++ index.js
@@ -38,6 +38,34 @@ app.get("/obtenerEquipos",function (peticion,respuesta) {
 		{
 			console.log("A ocurrido un error al traer datos del API");
 		}
+		});	
+	});
+
+	app.get("/datosliga",function (peticion,respuesta) {
+	var liga=peticion.query.id;
+	var f=new Date();
+	var mes=0;
+	if((f.getMonth()+1)<10){
+		mes='0'+(f.getMonth()+1);
+	}
+	var fecha=f.getFullYear()+'-'+mes+'-'+f.getDate();
+	var fecha2=(f.getFullYear()-1)+'-'+mes+'-'+f.getDate();
+	var options = {
+    url     : 'http://api.football-data.org/v2/competitions/'+liga+'/matches/?dateFrom='+fecha2+'&dateTo='+fecha,
+    method  : 'GET',
+    jar     : true,
+    headers : headers
+	}
+	request(options, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			valor=JSON.parse(body);
+			respuesta.send(valor);
+		}
+		else
+		{
+			console.log("A ocurrido un error al traer datos del API");
+			console.log(options.url);
+		}
 			
 	});
 		
