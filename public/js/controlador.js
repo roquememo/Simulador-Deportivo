7ffbce978b1353cb05960d5ac3bdcf9fdd9084ba
Guillermo Roque <roquememo@hotmail.com>
2018-08-20 18:05:00 -0600

    mejorando interfaz

--- public/js/controlador.js
+++ public/js/controlador.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+	$('#divbotones').hide();
 	//menu 
 	$(".menu-bar").on('click', function(e){
         e.preventDefault();
@@ -10,8 +10,17 @@ $(document).ready(function () {
 
 	//cargar equipos al seleccionar una liga
 	 $('#slc-Liga').unbind('change').bind('change', function (e){
+	 	cargarEquipos();
+    	
+  });
+	
+});
+
+
+
+	 function cargarEquipos() {
 	 	var id=$('#slc-Liga').val();
-    	$.ajax({
+	 	$.ajax({
 		url:'/obtenerEquipos',
 		method:'GET',
 		data:'id='+id,
@@ -31,10 +40,32 @@ $(document).ready(function () {
 			alert('Error al cargar valores');
 		}
 	});
-  });
+	 }
 
-	
+$('#btnSimular').click(function(){
+	 	$('#divbotones').show();
+	 	$('#divboton').hide();
+	 	$('#divlocal').removeClass('col-lg-5');
+	 	$('#divlocal').addClass('col-lg-6');
+	 	$('#divvisitante').removeClass('col-lg-5');
+	 	$('#divvisitante').addClass('col-lg-6');
 
+	 });
 
-	
-});
+	$('#btnSimular2').click(function(){
+	 	
+	 });
+
+	$('#btnReiniciar').click(function(){
+	 	$('#divbotones').hide();
+	 	$('#divboton').show();
+	 	$("#slc-local").html('<option selected>Seleccione equipo...</option>');
+		$("#slc-visitante").html('<option selected>Seleccione equipo...</option>');
+		var id=$('#slc-Liga').val();
+	 	$('#slc-Liga').val(id);
+	 	$('#divlocal').removeClass('col-lg-6');
+	 	$('#divlocal').addClass('col-lg-5');
+	 	$('#divvisitante').removeClass('col-lg-6');
+	 	$('#divvisitante').addClass('col-lg-5');
+	 	cargarEquipos();
+	 });
